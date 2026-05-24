3ace8aeebbcf12bd8b26d5cf960ebb8e8b0ad9e4
Bibek <bistabibek9@gmail.com>
2026-05-24 10:00:47 +0545

    fix: added new theme color for website

--- src/constants/color.ts
+++ src/constants/color.ts
@@ -1,3 +1,18 @@
 // export const WEBSITE_THEME_COLOR = "#0D6944";
 
-export const WEBSITE_THEME_COLOR = "#7D0E0E"
+// constants/color.ts
+export const WEBSITE_THEME_COLOR = "#7D0E0E";
+
+export const BRAND_COLORS = {
+    50: "#FDF2F2",
+    100: "#FAE2E2",
+    200: "#F4BCBC",
+    300: "#E98888",
+    400: "#D95050",
+    500: "#B52222",
+    600: "#7D0E0E", // WEBSITE_THEME_COLOR
+    700: "#5E0A0A",
+    800: "#430707",
+    900: "#2D0505",
+    950: "#1A0303",
+} as const;
